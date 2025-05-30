ed6f1a260e7410aa41b38715881ecfba26da0e20
ranger <ranger@gmail.com>
2025-05-30 15:12:03 -0400

    added basic logic to Blackjack game to simulate a deck of cards, also added some text to the game and allowed for user interactations

---
+++ card.py
@@ -0,0 +1,109 @@
+#list of imports
+import random , itertools
+import curses
+from curses import wrapper
+import time
+
+#main game loop for blackjack
+def gameloop(stdscr):
+    stdscr.clear()
+    rounds = 0
+    score = 0
+    legend = 0
+    risk = 0
+
+#this will make our deck and return the cards we need
+    def deck():
+    
+    #dictonary of suits
+        x = {
+            "H":"Hearts",
+            "D":"Diamonds",
+            "C":"Clubs",
+            "S":"Spades"
+        }
+    
+    #dictonary of numbers and face cards
+        y = {
+            "A":"Ace",
+            "2":"2",
+            "3":"3",
+            "4":"4",
+            "5":"5",
+            "6":"6",
+            "7":"7",
+            "8":"8",
+            "9":"9",
+            "J":"Jack",
+            "Q":"Queen",
+            "K":"King"
+            }
+   
+    #this makes an iterative list of the keys from both x and y dictionaries simulating all cards in a deck
+        deck = list(itertools.product(y.keys(),x.keys()))
+        
+        #shuffles the deck but at this stage its just the pairs of keys being shuffled
+        random.shuffle(deck)
+        
+        #this gives us a random pair of keys from the deck that were shuffled
+        #might be overkill but each person can on have 5 cards per game of blackjack so we have 10 cards
+        card1 = deck.pop()
+        card2 = deck.pop()
+        card3 = deck.pop()
+        card4 = deck.pop()
+        card5 = deck.pop()
+        card6 = deck.pop()
+        card7 = deck.pop()
+        card8 = deck.pop()
+        card9 = deck.pop()
+        card10 = deck.pop()
+        
+        #this is a formated string that takes the keys that were shuffled and gives us the value of both and then we format it like "The king of Spades"
+        a = f"The {y[card1[0]]} of {x[card1[1]]}"
+        b = f"The {y[card2[0]]} of {x[card2[1]]}"
+        c = f"The {y[card3[0]]} of {x[card3[1]]}"
+        d = f"The {y[card4[0]]} of {x[card4[1]]}"
+        e = f"The {y[card5[0]]} of {x[card5[1]]}"
+        f = f"The {y[card6[0]]} of {x[card6[1]]}"
+        g = f"The {y[card7[0]]} of {x[card7[1]]}"
+        h = f"The {y[card8[0]]} of {x[card8[1]]}"
+        i = f"The {y[card9[0]]} of {x[card9[1]]}"
+        j = f"The {y[card10[0]]} of {x[card10[1]]}"
+        
+        #picks a random card from the random shuffled cards that are already randomly picked and returns 1
+        cardList = [a,b,c,d,e,f,g,h,i,j]
+        newCard = random.choice(cardList)
+        return newCard
+        
+        
+        
+        
+    key = stdscr.getch()
+    stdscr.addstr(0,0,"Welcome to the Blackjack table!!!")
+    stdscr.addstr(0,2,"Press 1 to continue")
+    stdscr.refresh()
+    if key == ord('1'):
+        stdscr.clear()
+        stdscr.addstr(0,0,"Alright Kidd the rules are simple.")
+        stdscr.addstr(2,0,"First or closest to 21 wins")
+        stdscr.addstr(4,0,"Buts theres a catch!!!")
+        stdscr.addstr(6,0,"This game is about risk tolerance")
+        stdscr.addstr(8,0,"So if you want to become a legend in this casino...")
+        stdscr.addstr(10,0,"You are gonna need to take some pretty HIGH risk")
+        stdscr.addstr(12,0,"Press 1 Kidd, lets see if you got what it takes!!")
+        stdscr.refresh()
+        key1 = stdscr.getch()
+        stdscr.clear()
+        if key1 == ord('1'):
+            while rounds <= 2:
+                card = deck()
+                stdscr.clear()
+                stdscr.addstr(0,0,"Take em ...")
+                stdscr.addstr(0,2,f"{card}" )
+                stdscr.refresh()
+                rounds += 1
+       
+        
+        
+
+wrapper(gameloop)
